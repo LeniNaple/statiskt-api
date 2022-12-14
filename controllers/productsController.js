a020a5004e06f2ff00304175a729d3b5343c827b
Jesper <jesperkun@gmail.com>
2022-12-14 20:37:10 +0100

    works

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,24 +5,85 @@ const productSchema = require('../schemas/productSchemas')
 
 // Unsecured 
 controller.route('/').get(async (req, res) => {
-    try {
-        res.status(200).json(await productSchema.find())
-    } catch {
+    const products = []
+    const list = await productSchema.find()
+    if (list) {
+        for(let product of list) {
+            products.push({
+                articleNumber: product._id,
+                name: product.name,
+                description: product.description,
+                rating: product.rating,
+                price: product.price,
+                category: product.category,
+                tag: product.tag,
+                imageName: product.imageName
+            })
+        }
+        res.status(200).json(products)
+    } else {
         res.status(400).json()
     }
 })
 
-controller.route('/:tag').get((req, res) => {
-    const products = productSchema.find(x => x.tag == req.params.tag)
-
+controller.route('/:tag').get(async (req, res) => {
+    const products = []
+    const list = await productSchema.find({tag: req.params.tag })
+    if (list) {
+        for(let product of list) {
+            products.push({
+                articleNumber: product._id,
+                name: product.name,
+                description: product.description,
+                rating: product.rating,
+                price: product.price,
+                category: product.category,
+                tag: product.tag,
+                imageName: product.imageName
+            })
+        }
+        res.status(200).json(products)
+    } else {
+        res.status(400).json()
+    }
 })
 
-controller.route('/:tag/:take').get((req, res) => {
-    
+controller.route('/:tag/:take').get(async (req, res) => {
+    const products = []
+    const list = await productSchema.find({tag: req.params.tag }).limit(req.params.take)
+    if (list) {
+        for(let product of list) {
+            products.push({
+                articleNumber: product._id,
+                name: product.name,
+                description: product.description,
+                rating: product.rating,
+                price: product.price,
+                category: product.category,
+                tag: product.tag,
+                imageName: product.imageName
+            })
+        }
+        res.status(200).json(products)
+    } else {
+        res.status(400).json()
+    }
 })
 
-controller.route('/details/:articleNumber').get((req, res) => {
-    
+controller.route('/product/details/:articleNumber').get(async (req, res) => {
+    const product = await productSchema.findById(req.params.articleNumber)
+    if (product) {
+        res.status(200).json({articleNumber: product._id,
+            name: product.name,
+            description: product.description,
+            rating: product.rating,
+            price: product.price,
+            category: product.category,
+            tag: product.tag,
+            imageName: product.imageName})
+    } else {
+        res.status(404).json()
+    }
 })
 
 
